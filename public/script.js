30a3ab7b68637b6f0e1887cd4fa688344e5a1d22
Parth Patel <maganp00gmail.com>
2022-01-30 10:52:17 -0500

    final

--- public/script.js
+++ public/script.js
@@ -1,14 +1,25 @@
+var data2, data3, data4, data5;
+
 d3.csv("/grade-dist.csv").then(function (data){
 
-
+  console.log(data);
     // For now only send one data for a graph
+
+    data2 = data[1]
+    data3 = data[2]
+    data4 = data[3]
+    data5 = data[4]
     data = data[0];
 
     nonGradeItem = ["Course", "Course Title", "Honor", "Instructor", "Number", "Section"];
-
+    console.log("data2 ", data2);
     for( item in nonGradeItem){
-        // console.log(nonGradeItem[item])
+        console.log(nonGradeItem[item])
         delete data[nonGradeItem[item]];
+        delete data2[nonGradeItem[item]];
+        delete data3[nonGradeItem[item]];
+        delete data4[nonGradeItem[item]];
+        delete data5[nonGradeItem[item]];
     }
 
     console.log(data);
@@ -17,17 +28,41 @@ d3.csv("/grade-dist.csv").then(function (data){
 
 })
 
-
+function findClass(data){
+    for (d in data){
+        return d["Course"];
+    }
+}
 
 function generatePieChart( data){
 
+
     var chartData = [];
+    var chartData2 = [];
+    var chartData3 = [];
+    var chartData4 = [];
+    var chartData5 = [];
     for(d in data){
         chartData.push({x:d,value:data[d]})
     }
 
-    console.log(chartData)
+    for(d in data2){
+        chartData2.push({x:d,value:data2[d]})
+    }
 
+    for(d in data3){
+        chartData3.push({x:d,value:data3[d]})
+    }
+
+    for(d in data4){
+        chartData4.push({x:d,value:data4[d]})
+    }
+
+    for(d in data5){
+        chartData5.push({x:d,value:data5[d]})
+    }
+
+    console.log(chartData)
 
     anychart.onDocumentReady(function(){
 
@@ -44,7 +79,7 @@ function generatePieChart( data){
         var chart2 = anychart.pie();
 
         chart2.title("ENGL 2120");
-        chart2.data(chartData)
+        chart2.data(chartData2)
 
         chart2.container('container')
         chart2.draw()
@@ -53,7 +88,7 @@ function generatePieChart( data){
         var chart3 = anychart.pie();
 
         chart3.title("CPSC 2150");
-        chart3.data(chartData)
+        chart3.data(chartData3)
 
         chart3.container('container')
         chart3.draw()
@@ -62,7 +97,7 @@ function generatePieChart( data){
         var chart4 = anychart.pie();
 
         chart4.title("STAT 3090");
-        chart4.data(chartData)
+        chart4.data(chartData4)
 
         chart4.container('container')
         chart4.draw()
@@ -72,12 +107,14 @@ function generatePieChart( data){
         var chart5 = anychart.pie();
 
         chart5.title("BIOL 1040");
-        chart5.data(chartData)
+        chart5.data(chartData5)
 
         chart5.container('container')
         chart5s.draw()
 
 
     });
+
+
 
 }
